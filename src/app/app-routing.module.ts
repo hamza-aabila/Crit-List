7eb7a19c8c3bdba3afcd40f0055e1f8165568ec6
Hamza Aabila <hamza.aabila@gmail.com>
2019-08-05 17:19:32 +0100

    init

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'crit', pathMatch: 'full' },
+  { path: 'crit', loadChildren: './crit/crit.module#CritPageModule' },
+  { path: 'later', loadChildren: './later/later.module#LaterPageModule' },
+  { path: 'done', loadChildren: './done/done.module#DonePageModule' },
+  { path: 'profile', loadChildren: './profile/profile.module#ProfilePageModule' },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyAsvzcbyXTxcRrbhtQLNSoAhfEgVbvzyIQ",
+    authDomain: "crit-list-ada66.firebaseapp.com",
+    databaseURL: "https://crit-list-ada66.firebaseio.com",
+    projectId: "crit-list-ada66",
+    storageBucket: "",
+    messagingSenderId: "201882789674",
+    appId: "1:201882789674:web:0b2067b149b8e989"
+  }
+};
